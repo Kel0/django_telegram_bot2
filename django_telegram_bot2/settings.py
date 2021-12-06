687aab550786ff12fe2b3a4bf9e50b9467e9d30f
Tofu <61379597+Kel0@users.noreply.github.com>
2021-12-06 22:30:49 +0600

    db mod

--- django_telegram_bot2/settings.py
+++ django_telegram_bot2/settings.py
@@ -74,22 +74,22 @@ WSGI_APPLICATION = 'django_telegram_bot2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.getenv("DB_NAME"),
-#         'USER': os.getenv("USER"),
-#         'PASSWORD': os.getenv("PASSWORD"),
-#         'HOST': os.getenv("HOST"),
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "frdb",
+        'USER': "testuser",
+        'PASSWORD': "qwert",
+        'HOST': "localhost",
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
